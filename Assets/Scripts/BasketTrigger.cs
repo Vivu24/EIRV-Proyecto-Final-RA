4a1e01b857dc871aa12f5025565c8ce654891365
pokoli <psierr01@ucm.es>
2025-04-18 12:30:51 +0200

    Particulas celebracion

--- Assets/Scripts/BasketTrigger.cs
+++ Assets/Scripts/BasketTrigger.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 
 public class BasketTrigger : MonoBehaviour
 {
+    private ParticleSystem particleSystem;
+
     public int score = 0;
 
+    private void Start()
+    {
+        particleSystem = GameObject.FindWithTag("Celebration").GetComponent<ParticleSystem>();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
             score++;
             Debug.Log("Canasta! Puntuación: " + score);
+
+            if(particleSystem != null)
+                particleSystem.Play();
+
             Destroy(other.gameObject); // Opcional: destruir la pelota
         }
     }
